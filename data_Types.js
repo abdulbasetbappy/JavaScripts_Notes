3fc099d77a899b4ebad9c5cf47961f8ca46a826c
abdulbasetbappy <abdulbasetbappy@hotmial.com>
2023-03-27 02:17:13 +0600

    Commited Success

---
+++ data_Types.js
@@ -0,0 +1,18 @@
+//Primative Data Types
+/*
+var x ="hello World";    ------------------------string
+var x =25;---------------------------------------Number
+var x =true;-------------------------------------Boolean
+var x =null;-------------------------------------Null
+var x;-------------------------------------------Undifined
+*/
+//None - Premitive Data Types
+/*
+ var x =["helena", "Bappy", "sumiya", "mim"];----------------Array
+ var x ={
+    name: bappy,
+    age:25,
+    class: 10,
+    id:105546
+ };----------------------------------------------------------Object
+*/
